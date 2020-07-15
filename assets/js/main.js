8af13f688d7c1667498ebf9367abd82b2fb43e6f
YuhoHarada <yuhoharada0616aaa@gmail.com>
2020-07-15 15:14:31 +0200

    ver.1

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+let kette = document.getElementById("zeichenkette")
+let wort = document.getElementById("stichwort")
+let index 
+let resultVor = document.getElementById("resultVor")
+let resultHinter = document.getElementById("resultHinter")
+let davor = document.getElementById("davor")
+
+function trennen() {
+    index = kette.value.indexOf(wort.value)
+    if (index == -1) {
+        resultVor.innerHTML = "*** Das Zeichen könnte leicer nicht gefunden werden ***"
+        return false
+    }
+    if (davor.checked == true) {
+        resultVor.innerHTML = kette.value.slice(0, index)
+        resultHinter.innerHTML = kette.value.slice(index)
+    } else {
+        resultVor.innerHTML = kette.value.slice(0, index + wort.value.length)
+        resultHinter.innerHTML = kette.value.slice(index + wort.value.length)
+    }
+}
